a4a8bdb7a550d3b5d53f3b45675237c71dee1b03
SamHall14 <shall7@protonmail.com>
2021-07-16 23:31:21 -0700

    Add the add_five function.

--- R/somecode.R
+++ R/somecode.R
@@ -1,16 +1,16 @@
 #' @title Add two!
-#' 
+#'
 #' @param x Number to add two to!
 #' @return x + 2!
 #' @examples
 #' add_two(2) # returns 4
 #' @export
 add_two  <-  function(x) {
- return( x + 2 ) 
+ return( x + 2 )
 }
 
 #' @title Add four!
-#' 
+#'
 #' @description Add four, you mo'!
 #' @param x Number to add four to!
 #' @return x + 4
@@ -18,5 +18,17 @@ add_two  <-  function(x) {
 #' add_four(2) # returns 4
 #' @export
 add_four  <-  function(x) {
- return( x + 4 ) 
+ return( x + 4 )
+}
+
+#' @title Add five!
+#'
+#' @description Add five, you mo'!
+#' @param x Number to add five to!
+#' @return x + 5
+#' @examples
+#' add_five(2) # returns 4
+#' @export
+add_five  <-  function(x) {
+ return( x + 5 )
 }

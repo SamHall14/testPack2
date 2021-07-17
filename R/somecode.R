d990d274228235c6ac0d5fa14ba480b890ba39d8
SamHall14 <shall7@protonmail.com>
2021-07-16 23:33:02 -0700

    Remove add_five function

--- R/somecode.R
+++ R/somecode.R
@@ -21,14 +21,14 @@ add_four  <-  function(x) {
  return( x + 4 )
 }
 
-#' @title Add five!
-#'
-#' @description Add five, you mo'!
-#' @param x Number to add five to!
-#' @return x + 5
-#' @examples
-#' add_five(2) # returns 4
-#' @export
-add_five  <-  function(x) {
- return( x + 5 )
-}
+##' @title Add five!
+##'
+##' @description Add five, you mo'!
+##' @param x Number to add five to!
+##' @return x + 5
+##' @examples
+##' add_five(2) # returns 4
+##' @export
+#add_five  <-  function(x) {
+# return( x + 5 )
+#}

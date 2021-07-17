39e8b68c9c6e559e4124040f6d9e3b86f561c104
SamHall14 <shall7@protonmail.com>
2021-07-16 21:53:45 -0700

    modified:   R/somecode.R

--- R/somecode.R
+++ R/somecode.R
@@ -8,3 +8,14 @@
 add_two  <-  function(x) {
  return( x + 2 ) 
 }
+
+#' @title Add four!
+#' 
+#' @param x Number to add four to!
+#' @return x + 4
+#' @examples
+#' add_four(2) # returns 4
+#' @export
+add_four  <-  function(x) {
+ return( x + 4 ) 
+}
